18849d932757f9a4df5895c9239ea9860b11aa6e
Mariami Tchanturia <mariam.tchanturia1994@gmail.com>
2023-03-27 01:26:16 +0400

    add unit tests for Counter component

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import Counter from './Counter';
+
+
+describe('Counter component tests:', () => {
+
+	it('Should render a counter with passed initial value', () => {
+		const initialValue = 100;
+		render(<Counter initialValue={initialValue} />);
+
+		const value = screen.getByText(`${initialValue}`);
+		expect(value).toBeInTheDocument();
+	});
+
+	it('Should increase count by one, when Increment button is clicked', () => {
+		const initialValue = -7;
+		render(<Counter initialValue={initialValue} />);
+
+		fireEvent.click(screen.getByText(/increment/i));
+		expect(screen.getByText(`${initialValue + 1}`)).toBeInTheDocument();
+
+	});
+
+	it('Should decrease count by one, when Decrement button is clicked', () => {
+		const initialValue = 4;
+		render(<Counter initialValue={initialValue} />);
+
+		fireEvent.click(screen.getByText(/decrement/i));
+		expect(screen.getByText(`${initialValue - 1}`)).toBeInTheDocument();
+	});
+
+});
